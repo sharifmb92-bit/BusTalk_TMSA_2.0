16bc823829cee7cc6553a64cba843763b5da8124
sharifmb92-bit <270324566+sharifmb92-bit@users.noreply.github.com>
2026-06-25 11:59:23 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = 'bustalk-v2';
+const ASSETS = ['./', './index.html', './icon-152.png', './icon-192.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+// medbasha
